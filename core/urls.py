40cad3aba9e2b01eb0d2aa43874c885d03871756
rusplane01 <99131986+rusplane01@users.noreply.github.com>
2025-03-12 20:42:48 +0200

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -25,6 +25,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('auth_sys/', include('auth_sys.urls')),
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path('user_payment', include('user_payment.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
